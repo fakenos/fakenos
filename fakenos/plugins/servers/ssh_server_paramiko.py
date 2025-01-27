3bb5c23c7fafa1c07892ad02ecdb0616dcfd841f
evilmonkey19 <eperpinyapitarch@gmail.com>
2025-01-27 19:12:13 +0100

    Implementing backspace. Fixes #18 Implemented backspace on the shell. Temporarly, the arrows have been disabled and in order to ensure more stability, it has been limited the number of characters recognized.

--- fakenos/plugins/servers/ssh_server_paramiko.py
+++ fakenos/plugins/servers/ssh_server_paramiko.py
@@ -146,26 +146,47 @@ def channel_to_shell_tap(channel_stdio, shell_stdin, shell_replied_event, run_sr
             log.error("SSH channel is not active. Exiting.")
             break
         try:
-            if byte in (b"\r", b"\n"):
-                channel_stdio.write(b"\r\n")
-                log.debug("ssh_server.channel_to_shell_tap echoing new line to channel: %s", [b"\r\n"])
-                buffer.write(byte)
-                buffer.seek(0)
-                line = buffer.read().decode(encoding="utf-8")
-                buffer.seek(0)
-                buffer.truncate()
-                log.debug("ssh_server.channel_to_shell_tap sending line to shell: %s", [line])
-                shell_stdin.write(line)
-                shell_replied_event.clear()
-            else:
-                channel_stdio.write(byte)
-                log.debug("ssh_server.channel_to_shell_tap echoing byte to channel: %s", [byte])
-                if byte not in [b"\x00", b""]:
+            match byte:
+                case b'\x1b':  # arrows
+                    channel_stdio.read(2)
+                case b'\r' | b'\n':
+                    channel_stdio.write(b"\r\n")
+                    log.debug("ssh_server.channel_to_shell_tap echoing new line to channel: %s", [b"\r\n"])
                     buffer.write(byte)
+                    buffer.seek(0)
+                    line = buffer.read().decode(encoding="utf-8")
+                    buffer.seek(0)
+                    buffer.truncate()
+                    log.debug("ssh_server.channel_to_shell_tap sending line to shell: %s", [line])
+                    shell_stdin.write(line)
+                    shell_replied_event.clear()
+                case b'\x7f':   # backspace
+                    if buffer.tell() > 0:
+                        channel_stdio.write(b"\b \b")
+                        log.debug("ssh_server.channel_to_shell_tap echoing backspace to channel: %s", [b"\b \b"])
+                        buffer.seek(buffer.tell() - 1)
+                        buffer.truncate()
+                case _:
+                    channel_stdio.write(byte)
+                    log.debug("ssh_server.channel_to_shell_tap echoing byte to channel: %s", [byte])
+                    if __is_byte_permitted(byte):
+                        buffer.write(byte)
             time.sleep(0.01)
         except (OSError, EOFError) as e:
             log.error("ssh_server.channel_to_shell_tap channel write error: %s", e)
             break
+
+
+def __is_byte_permitted(byte: bytes) -> bool:
+    """
+    Method to return the list of permitted bytes
+    """
+    permitted_bytes = []
+    permitted_bytes += [bytes([i]) for i in range(48, 58)]  # b'0' to b'9'
+    permitted_bytes += [bytes([i]) for i in range(65, 91)]  # b'A' to b'Z'
+    permitted_bytes += [bytes([i]) for i in range(97, 123)]  # b'a' to b'z'
+    permitted_bytes += [b'-', b' ', b'_', b'/', b'?']
+    return byte in permitted_bytes
 
 
 def shell_to_channel_tap(
--- tests/plugins/test_ssh_server_paramiko.py
+++ tests/plugins/test_ssh_server_paramiko.py
@@ -293,6 +293,58 @@ class ChannelToShellTapTest(unittest.TestCase):
         self.assertEqual(self.mock_run_srv.is_set.call_count, 2)
         self.assertEqual(self.mock_channel_stdio.read.call_count, 1)
 
+    def test_backspace_works_if_buffer_empty(self):
+        """Check that the backspace works when buffer is empty."""
+        self.mock_run_srv.is_set.side_effect = [True, False]
+        self.mock_channel_stdio.read.side_effect = [b'\x7f']
+        channel_to_shell_tap(
+            channel_stdio=self.mock_channel_stdio,
+            shell_stdin=self.mock_shell_stdin,
+            shell_replied_event=self.mock_shell_replied_event,
+            run_srv=self.mock_run_srv,
+        )
+        self.assertEqual(self.mock_channel_stdio.write.call_count, 0)
+
+    def test_backspace_works_if_buffer_not_empty(self):
+        """Check that the backspace works when buffer is not empty."""
+        self.mock_run_srv.is_set.side_effect = [True, True, False]
+        self.mock_channel_stdio.read.side_effect = [b'a', b'\x7f']
+        channel_to_shell_tap(
+            channel_stdio=self.mock_channel_stdio,
+            shell_stdin=self.mock_shell_stdin,
+            shell_replied_event=self.mock_shell_replied_event,
+            run_srv=self.mock_run_srv,
+        )
+        self.assertEqual(self.mock_channel_stdio.write.call_count, 2)
+        self.mock_channel_stdio.write.assert_called_with(b'\x08 \x08')
+
+    def test_arrow_keys_does_not_work(self):
+        """ Check that when pressing any arrow key, the cursor does not move. """
+        self.mock_run_srv.is_set.side_effect = [True] * 4 + [False]
+        self.mock_channel_stdio.read.side_effect = \
+            [b'\x1b', b'[A', b'\x1b', b'[B', b'\x1b', b'[C', b'\x1b', b'[D']
+        channel_to_shell_tap(
+            channel_stdio=self.mock_channel_stdio,
+            shell_stdin=self.mock_shell_stdin,
+            shell_replied_event=self.mock_shell_replied_event,
+            run_srv=self.mock_run_srv,
+        )
+        self.mock_channel_stdio.read.assert_has_calls([mock.call(1), mock.call(2)] * 4)
+        self.mock_shell_stdin.write.assert_not_called()
+        self.mock_channel_stdio.write.assert_not_called()
+
+    def test_help_or_question_mark_goes_to_help(self):
+        """ Check that when pressing 'help' or '?' the shell goes to the help command. """
+        self.mock_run_srv.is_set.side_effect = [True] * 2 + [False]
+        self.mock_channel_stdio.read.side_effect = [b'?', b'\n']
+        channel_to_shell_tap(
+            channel_stdio=self.mock_channel_stdio,
+            shell_stdin=self.mock_shell_stdin,
+            shell_replied_event=self.mock_shell_replied_event,
+            run_srv=self.mock_run_srv,
+        )
+        self.mock_shell_stdin.write.assert_called_once_with("?\n")
+
 
 class ShellToChannelTapTest(unittest.TestCase):
     """
